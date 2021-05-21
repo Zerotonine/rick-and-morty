d4a1045b8d758d6b0fcea7cbf9d523649c43587b
Pierre Philipp Dubois <p.dubois@socialpoints.online>
2021-05-21 11:22:13 +0200

    SSG demo stuff added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import {Box, Container, Text, Wrap, WrapItem} from "@chakra-ui/react"
+import {Box, Container, Text, Wrap, WrapItem, Switch, Center} from "@chakra-ui/react"
 import {getCharacters} from "./api/api"
 import React, {useState} from "react"
 
 export default function Home({data}) {
   const [chars, setChars] = useState(data.props.characters);
+  const [staticPage, setStaticPage] = useState(false);
 
   return (
     <div>
@@ -28,6 +29,10 @@ export default function Home({data}) {
           >
             Ich bin eine Gurke!
           </Text>
+          <Center>
+            <Text mb="1rem" mx="5px">load static pages?</Text>
+            <Switch size="lg" onChange={() => setStaticPage(!staticPage)} mb="1rem"/>
+          </Center>
         </Container>
 
         <Wrap px="1.2rem" spacing={4} justify="center">
@@ -41,9 +46,10 @@ export default function Home({data}) {
               lineHeight="0"
               _hover={{boxShadow: "4px 4px 10px 3px #615136"}}
             >
-              <Link href={`/characters/${char.id}`}>
+              <Link href={staticPage ? `/static/${char.id}` : `/characters/${char.id}`}>
                 <a>
-                  <Image src={char.image} alt={char.name} width="400px" height="400px"/>
+                  {/* <Image src={char.image} alt={char.name} width="400px" height="400px"/> */}
+                  <img src={char.image} alt={char.name} width="400px" height="400px" />
                 </a>
               </Link>
             </WrapItem>
